c4f4732cd8276f9c917c1263303ee4399f5b9155
Ryo Nakamine <rnakamine8080@gmail.com>
2021-07-25 00:18:50 +0900

    Add acm validation

--- acm.tf
+++ acm.tf
@@ -7,3 +7,8 @@ resource "aws_acm_certificate" "example" {
     create_before_destroy = true
   }
 }
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.example.arn
+  validation_record_fqdns = [for record in aws_route53_record.example_certificate : record.fqdn]
+}
