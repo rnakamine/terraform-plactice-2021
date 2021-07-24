c4b2f587d09715a7eb65ca664615c093e6695254
Ryo Nakamine <rnakamine8080@gmail.com>
2021-07-24 23:23:03 +0900

    Add acm

--- route53.tf
+++ route53.tf
@@ -18,3 +18,18 @@ resource "aws_route53_record" "example" {
 output "domain_name" {
   value = aws_route53_record.example.name
 }
+
+resource "aws_route53_record" "example_certificate" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  zone_id = data.aws_route53_zone.example.id
+  ttl     = 60
+}
